125305e1655ab9b8313793446d084e0361162ed2
Thilina Satoru <satoru.thilina@gmail.com>
2024-08-12 20:41:01 +0530

    screen loading text added. code cleanups.

--- device/device.ino
+++ device/device.ino
@@ -18,36 +18,19 @@ unsigned long weightStableTime = 0; /////
 bool weightStable = false;          /////
 /////////////////////////////////////////
 
-// Create display and scale objects  ////////////////////////////////
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); ///
-HX711 scale;                                                      ///
-WiFiClient wifiClient;                                            ///
-/////////////////////////////////////////////////////////////////////
+// Create display and scale objects  ////////////////////////////////////////
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); ///
+HX711 scale;                                                              ///
+WiFiClient wifiClient;                                                    ///
+/////////////////////////////////////////////////////////////////////////////
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
 ////////////////////////////////                SETUP              ////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void setup() {
+void setup() { ////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.begin(115200);
-
-  //// Initialize scale ////////////////////////////////
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  ///
-  scale.set_scale(scale_calibrate);  ///////////////////
-  scale.tare();  ///////////////////////////////////////
-  //////////////////////////////////////////////////////
-
-  // Initialize Wi-Fi  ///////////////////////////
-  WiFi.begin(ssid, password);                /////
-  while (WiFi.status() != WL_CONNECTED) {    /////
-    delay(1000);                             /////
-    Serial.println("Connecting to WiFi..."); /////
-  }                                          /////
-  Serial.println("Connected to WiFi"); ///////////
-  Serial.print("Device IP Address: "); ///////////
-  Serial.println(WiFi.localIP()); ////////////////
-  ////////////////////////////////////////////////
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
@@ -55,19 +38,37 @@ void setup() {
   //// Initialize display //////////////////////////////
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  ///
     Serial.println(F("SSD1306 allocation failed"));  ///
-    for (;;);  /////////////////////////////////////////
-  }  ///////////////////////////////////////////////////
-  delay(1000);  ////////////////////////////////////////
-  DisplayModeItemCounter();  ///////////////////////////
+    for (;;);                                        ///
+  }                                                  ///
   //////////////////////////////////////////////////////
+
+  //// Initialize scale ////////////////////////////////
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  ///
+  scale.set_scale(scale_calibrate);                  ///
+  scale.tare();                                      ///
+  //////////////////////////////////////////////////////
+
+  // Initialize Wi-Fi  ///////////////////////////
+  WiFi.begin(ssid, password);                /////
+  while (WiFi.status() != WL_CONNECTED) {    /////
+    displayCenteredText("WiFi...");          /////
+    delay(1000);                             /////
+    Serial.println("Connecting to WiFi..."); /////
+  }                                          /////
+  Serial.println("Connected to WiFi");       /////
+  Serial.print("Device IP Address: ");       /////
+  Serial.println(WiFi.localIP());            /////
+  ////////////////////////////////////////////////
+
+  displayCenteredText("Ready!");
 }
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
 ////////////////////////////////                LOOP               ////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void loop() {
+void loop() { /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   /// Ceck push button switch mode
   bool currentSwitchState = digitalRead(BUTTON_PIN) == LOW;
   if (currentSwitchState != isCashierMode) {
@@ -126,27 +127,15 @@ void loop() {
       }
     }
   }
-
-  delay(10);
+  delay(50);
 }
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
 ////////////////////////////////              FUNCTIONS            ////////////////////////////////////////////////////////
 ////////////////////////////////                                   ////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Function to clear the display and fill it with black color
-void clearDisplayBlack() {
-  display.clearDisplay();
-  display.fillScreen(BLACK);
-  display.display();
-}
-
-// Function to get the width of the text in pixels
-int getTextWidth(const char* text, int textSize) {
-  return strlen(text) * 6 * textSize;
-}
 
 // Function to display the item counter mode on the OLED display
 void DisplayModeItemCounter() {
@@ -162,7 +151,7 @@ void DisplayModeItemCounter() {
   display.print(counter);
 
   display.setTextSize(1);
-  display.setCursor((SCREEN_WIDTH - getTextWidth(String(totalWeight).c_str(), 1)) / 2, 55);
+  display.setCursor((SCREEN_WIDTH - getTextWidth(String(totalWeight).c_str(), 1)) / 2, 50);
   display.print(totalWeight);
   display.print(" g");
 
@@ -232,6 +221,50 @@ void DisplayModeCashier(int count) {
   } else {
     Serial.println("WiFi Disconnected");
   }
+}
+
+// Function to clear the display and fill it with black color
+void clearDisplayBlack() {
+  display.clearDisplay();
+  display.fillScreen(BLACK);
+  display.display();
+}
+
+// Function to get the width of the text in pixels
+int getTextWidth(const char* text, int textSize) {
+  return strlen(text) * 6 * textSize;
+}
+
+// Function to print text on OLED screen, maximized and centered
+void displayCenteredText(const char* text) {
+  clearDisplayBlack();
+  // Start with the maximum font size
+  int textSize = 1;
+  int textWidth = getTextWidth(text, textSize);
+  // Find the maximum font size that fits the screen width
+  while (textWidth <= SCREEN_WIDTH && textSize <= 10) {
+    textSize++;
+    textWidth = getTextWidth(text, textSize);
+  }
+  // Step back to the last fitting size
+  textSize--;
+  textWidth = getTextWidth(text, textSize);
+  // Calculate the x and y position to center the text
+  int x = (SCREEN_WIDTH - textWidth) / 2;
+  int y = (SCREEN_HEIGHT - (8 * textSize)) / 2; // Each line is 8 pixels high in default font
+  // Set text size and print text
+  display.setTextSize(textSize);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(x, y);
+  display.print(text);
+  display.display();
+}
+
+void restartESP() {
+  // Reset the NodeMCU after 3 seconds
+  delay(3000);
+  Serial.println("Restarting NodeMCU...");
+  ESP.restart();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
